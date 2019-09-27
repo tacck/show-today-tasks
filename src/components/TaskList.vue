1650531473acafff3d27e399b946dc6ceda14e9f
Takuya KIHARA <gray@tacck.net>
2019-09-27 11:08:37 +0900

    :lipstick: タップでアイテムを消す

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,20 +1,30 @@
 <template>
   <v-container fluid>
-    <v-data-iterator :items="lists" hide-default-footer>
+    <v-data-iterator
+      :items="lists"
+      hide-default-footer
+    >
       <template v-slot:header>
-          <v-toolbar
-            class="mb-2"
-            color="indigo darken-5"
-            dark
-            flat
-          >
-            <v-toolbar-title>Today's Tasks</v-toolbar-title>
-          </v-toolbar>
-        </template>
+        <v-toolbar
+          class="mb-2"
+          color="indigo darken-5"
+          dark
+          flat
+        >
+          <v-toolbar-title>Today's Tasks</v-toolbar-title>
+        </v-toolbar>
+      </template>
       <template v-slot:default="props">
         <v-row>
-          <v-col v-for="item in props.items" :key="item.name" cols="12" sm="6" md="4" lg="3">
-            <v-card>
+          <v-col
+            v-for="item in props.items"
+            :key="item.name"
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+          >
+            <v-card :id="item.id" v-on:click="finish( item.id )">
               <v-card-title>
                 <h4>{{ item.name }}</h4>
               </v-card-title>
@@ -34,23 +44,30 @@
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios'
 
 export default {
-  props: ["listId", "apiKey", "apiToken"],
-  data: function() {
+  props: ['listId', 'apiKey', 'apiToken'],
+  data: function () {
     return {
       itemsPerPageOptions: [],
       itemsPerPage: 5,
       lists: []
-    };
+    }
   },
-  mounted: async function() {
+  mounted: async function () {
     const response = await axios.get(
       `https://api.trello.com/1/lists/${this.listId}/cards?fields=all&key=${this.apiKey}&token=${this.apiToken}`
-    );
-    console.log(response);
-    this.lists = response.data;
+    )
+    console.log(response)
+    this.lists = response.data
+  },
+  methods: {
+    finish: function (id) {
+      console.log(id)
+      window.interactiveCanvas.sendTextQuery(`finishedTask-${id}`)
+      this.lists = this.lists.filter((list) => list.id !== id)
+    }
   }
-};
+}
 </script>
